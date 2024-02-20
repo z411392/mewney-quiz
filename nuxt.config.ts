5edbfbfcf80c2f0f37421a4b57ecaff251f4e13c
Jun Wei, Huang <z411392@gmail.com>
2024-02-20 17:26:27 +0800

    feat: 引入 vuetify 和 tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,32 @@ const modules: string[] = [
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules,
+  webpack: { optimizeCSS: true },
+  vite: { build: { chunkSizeWarningLimit: 1600 } },
+  build: { transpile: ['vuetify'] },
+  app: {
+    head: {
+      title: '',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/png', href: "data:image/png;base64,iVBORw0KGgo=" },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700' },
+      ],
+      script: [
+        { src: 'https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver,ResizeObserver,WebAnimations,Object.fromEntries,Array.prototype.at' }
+      ],
+    },
+  },
+  css: [
+    '@mdi/font/css/materialdesignicons.min.css',
+    '~/public/css/main.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ plugins/vuetify.ts
@@ -0,0 +1,32 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { zhHant, en } from 'vuetify/locale'
+import 'vuetify/styles'
+import type { Plugin } from "nuxt/app"
+import { Settings } from 'luxon'
+
+import type { ThemeDefinition } from 'vuetify'
+const theme: ThemeDefinition = {
+    dark: false,
+    variables: {
+        'theme-overlay-multiplier': 1,
+        'activated-opacity': 0,
+    }
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        // ssr: false,
+        components: { ...components },
+        directives,
+        theme: { defaultTheme: 'theme', themes: { theme } },
+        locale: {
+            locale: 'zhHant',
+            fallback: 'en',
+            messages: { zhHant, en },
+        },
+    })
+    Settings.defaultZone = "Asia/Taipei"
+    nuxtApp.vueApp.use(vuetify)
+}) as Plugin
